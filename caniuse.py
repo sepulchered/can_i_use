9e801d3a7e6a0c848b637acdf05e297f67d3d380
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2015-02-11 10:21:51 +0300

    add getting proper data from github account of can i use

--- caniuse.py
+++ caniuse.py
@@ -1,10 +1,16 @@
+import sys
+import json
 import argparse
+
+import requests
 
 
 class CanIUse():
     def __init__(self, feature=None, browsers=None):
         self.feature = feature
         self.browsers = browsers if browsers else []
+        self.support_data = None
+        self.data_url = 'https://raw.githubusercontent.com/Fyrd/caniuse/master/data.json'
 
     def parse_args(self):
         parser = argparse.ArgumentParser(description='Check if you can use ' \
@@ -13,10 +19,20 @@ class CanIUse():
         parser.add_argument('--in', dest='browsers', nargs='*', help="browsers to check for")
         parser.parse_args(namespace=self)
 
-    def _get_data(self):
-        # here get data of support for features
-        # look at https://github.com/Fyrd/caniuse/blob/master/data.json
-        pass
+    def _get_data(self, exit_on_fail=True):
+        r = requests.get(self.data_url)
+        if r.status_code != 200:
+            if exit_on_fail:
+                sys.exit('Cannot get data from provided url [%s]' % self.data_url)
+            else:
+                return
+        try:
+            self.support_data = json.loads(r.text)
+        except ValueError:
+            if exit_on_fail:
+                sys.exit('Invalid data provided from url [%s]' % self.data_url)
+            else:
+                return
 
 
 if __name__ == '__main__':
---
+++ test.py
@@ -0,0 +1,36 @@
+import json
+import unittest
+from unittest.mock import Mock, patch
+
+import requests
+
+from caniuse import CanIUse
+
+
+class TestCanIUse(unittest.TestCase):
+    def setUp(self):
+        self.can_i_use = CanIUse()
+
+    def test_defaults(self):
+        self.assertEqual(self.can_i_use.browsers, [])
+        self.assertIsNone(self.can_i_use.feature)
+        self.assertIsNone(self.can_i_use.support_data)
+
+    def test_get_data(self):
+        valid_json = json.dumps({'test': 'gigla'})
+        invalid_json = 'gigla-migla'
+        fake_response = Mock()
+        attrs = {'status_code': 200, 'text': valid_json}
+        fake_response.configure_mock(**attrs)
+
+        with patch.object(requests, 'get', return_value=fake_response) as mock_get:
+            self.can_i_use._get_data()
+        self.assertTrue(mock_get.assert_called())
+        self.assertIsNotNone(self.can_i_use.support_data)
+
+        attrs = {'status_code': 200, 'text': invalid_json}
+        fake_response.configure_mock(**attrs)
+        with patch.object(requests, 'get', return_value=fake_response) as mock_get:
+            self.assertRaises(ValueError, self.can_i_use._get_data(exit_on_fail=False),
+                              'Invalid data provided from url [%s]' % self.can_i_use.data_url)
+        self.assertTrue(mock_get.assert_called())
